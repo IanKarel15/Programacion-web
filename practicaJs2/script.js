96e3fd84d93a347e3866a330962144bc125d781c
IanKarel15 <iankareld_23@alu.uabcs.mx>
2025-10-19 14:00:53 -0700

    Botones que cambian fondo de color

---
+++ practicaJs2/script.js
@@ -0,0 +1,7 @@
+const cuerpo = document.body;
+
+document.getElementById("rojo").onclick = () => cuerpo.style.backgroundColor = "red";
+document.getElementById("verde").onclick = () => cuerpo.style.backgroundColor = "green";
+document.getElementById("negro").onclick = () => cuerpo.style.backgroundColor = "black";
+document.getElementById("azul").onclick = () => cuerpo.style.backgroundColor = "blue";
+document.getElementById("regresar").onclick = () => cuerpo.style.backgroundColor = "white";
